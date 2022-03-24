e84eb1184d7511f23b376e40933c9845ba82aa97
Maria Baykova <mnbaikova@gmail.com>
2022-03-24 17:00:19 +0500

    +simple gallery

--- assets/script.js
+++ assets/script.js
@@ -35,9 +35,46 @@ let clearCalc = () => {
     document.getElementById("calculator__second-num").value = "";  
 }
 
+// блок демонстрации смены фонов дивов
+
 function changeBckg( ind ) {
     let colors = [ 'red', 'green', 'blue', 'yellow'];
     let list = document.getElementsByClassName("change-background__block");
     let colorPosNext = ( colors.indexOf(list[ind].style.background) + 1 > 3) ? 0 : colors.indexOf(list[ind].style.background) + 1;
     list[ind].style.background = colors[Number(colorPosNext)];
+}
+
+
+// блок галерея
+
+let galleryItems = document.getElementsByClassName("simple-gallery__item");
+console.log(galleryItems);
+for( let i = 1; i < galleryItems.length; i++ ) {
+    galleryItems[i].style.display = "none";
+}
+
+function showNextPicture(n) {
+    let galleryItems = document.getElementsByClassName("simple-gallery__item");
+    let i = 0;
+    for ( ; i < galleryItems.length; i++ ) {
+        if ( galleryItems[i].style.display == "none" ) {
+            continue;
+        }
+        let nextInd = i+n;
+        switch (nextInd) {
+            case -1:
+              nextInd = galleryItems.length - 1;
+              break;
+            case galleryItems.length:
+              nextInd = 0;
+              break;
+            default:
+              //
+          }
+        console.log("next ind " + nextInd);
+        galleryItems[i].style.display = "none";
+        galleryItems[nextInd].style.display = "";
+        break;
+    }
+
 }
