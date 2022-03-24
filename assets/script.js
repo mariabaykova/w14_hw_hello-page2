0f7c2942ebfca5e8260cb88b631ea06264bd3a01
Maria Baykova <mnbaikova@gmail.com>
2022-03-24 11:50:03 +0500

    +calc

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,34 @@ function showName() {
     document.getElementById("result").innerHTML = document.getElementById('your-name').value; 
     document.getElementById("res-name").value = document.getElementById('your-name').value;
 }
+
+let sum = () => {
+    document.getElementById("calculator__result").value = 
+    Number(document.getElementById("calculator__first-num").value) + 
+    Number(document.getElementById("calculator__second-num").value);
+}
+
+let mult = () => {
+    document.getElementById("calculator__result").value = 
+    Number(document.getElementById("calculator__first-num").value) *
+    Number(document.getElementById("calculator__second-num").value);
+
+}
+let division = () => {
+    document.getElementById("calculator__result").value = 
+    Number(document.getElementById("calculator__first-num").value) /
+    Number(document.getElementById("calculator__second-num").value);
+
+};
+let subtr = () => {
+    document.getElementById("calculator__result").value = 
+    Number(document.getElementById("calculator__first-num").value) -
+    Number(document.getElementById("calculator__second-num").value);
+
+};
+
+let clearCalc = () => {
+    document.getElementById("calculator__result").value = "";
+    document.getElementById("calculator__first-num").value = "";
+    document.getElementById("calculator__second-num").value = "";  
+}
