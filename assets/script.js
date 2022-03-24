2a923f60ba193da3a30910a7d5b7f6e2c8dcdd39
Maria Baykova <mnbaikova@gmail.com>
2022-03-24 15:56:30 +0500

    +changing backg

--- assets/script.js
+++ assets/script.js
@@ -34,3 +34,10 @@ let clearCalc = () => {
     document.getElementById("calculator__first-num").value = "";
     document.getElementById("calculator__second-num").value = "";  
 }
+
+function changeBckg( ind ) {
+    let colors = [ 'red', 'green', 'blue', 'yellow'];
+    let list = document.getElementsByClassName("change-background__block");
+    let colorPosNext = ( colors.indexOf(list[ind].style.background) + 1 > 3) ? 0 : colors.indexOf(list[ind].style.background) + 1;
+    list[ind].style.background = colors[Number(colorPosNext)];
+}
